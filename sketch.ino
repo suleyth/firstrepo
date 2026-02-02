c3266961285cca053a40a8c4c21fcb6449b391a7
suleyth <suleyth_c@tuta.com>
2026-02-02 12:17:24 +0000

    Modify Arduino sketch

--- sketch.ino
+++ sketch.ino
@@ -18,6 +18,8 @@
   by Colby Newman
   modified 10 Jan 2024
   by Mary Loftus
+  modified 30 Jan 2026
+  by suleyth
 
   This example code is in the public domain.
 
@@ -33,9 +35,9 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(500);                      // wait for half a second
   digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  delay(500);                      // wait for half a second
 
 }
 
